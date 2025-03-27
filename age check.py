92393d0a6208416a7872a2357a163da9dfb61435
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-03-27 17:05:13 +0300

    if loop in Python

---
+++ age check.py
@@ -0,0 +1,8 @@
+Tom_age = 10
+kindergaten_age = 5
+if Tom_age > kindergaten_age:
+    print("Tom should go to ppreschool")
+elif Tom_age == kindergaten_age:
+    print("Tom is old enough to go to kindergarten")
+else: 
+    print("Tom is not old enough to go to kindergarten")
